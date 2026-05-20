88eb5c3de210e80509fb8463ba816fba3281797e
jokate <kkkk4017@naver.com>
2026-05-20 21:52:54 +0900

    MNY-2 InputStateMachine 개설 / StateMachine 사용은 좋은데 몽타주에서 State 변경할 수 있도록 인풋을 받도록 하기 위함.

---
+++ MNYS/Private/AnimNotify/YSAnimNotify_TransitionState.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify/YSAnimNotify_TransitionState.h"
+
+#include "Input/StateMachine/YSInputStateMachineComponent.h"
+
+void UYSAnimNotify_TransitionState::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                           const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
+	AActor* OwnerActor = MeshComp->GetOwner();
+	
+	if ( IsValid(OwnerActor) )
+	{
+		UYSInputStateMachineComponent* StateMachineComponent = UYSInputStateMachineComponent::Get(OwnerActor);
+		
+		if ( IsValid(StateMachineComponent) )
+			StateMachineComponent->TransitionState(NextState);
+	}
+}
---
+++ MNYS/Public/AnimNotify/YSAnimNotify_TransitionState.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "General/YSEnum.h"
+#include "YSAnimNotify_TransitionState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MNYS_API UYSAnimNotify_TransitionState : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public : 
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	
+public : 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (DisplayName = "바꾸고자하는 State"))
+	EYSInputStatesType NextState;
+};
