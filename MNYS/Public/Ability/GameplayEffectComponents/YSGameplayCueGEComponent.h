765453b409067eec6435f3e4a84c3235e8d7722b
jokate <kkkk4017@naver.com>
2026-05-24 15:43:39 +0900

    MNY-3 스킬 슬롯 배정 및 저스트 회피에 따른 스킬 변경 처리. /  GameplayCue GE Component 추가.

---
+++ MNYS/Public/Ability/GameplayEffectComponents/YSGameplayCueGEComponent.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ability/GameplayEffectComponents/YSGameplayEffectComponent.h"
+#include "YSGameplayCueGEComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MNYS_API UYSGameplayCueGEComponent : public UYSGameplayEffectComponent
+{
+	GENERATED_BODY()
+	
+public :
+	virtual void OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const override;
+protected:
+	virtual void OnActiveGameplayEffectRemoved(const FGameplayEffectRemovalInfo& GameplayEffectRemovalInfo, FActiveGameplayEffectsContainer* ActiveGEContainer) const override;
+
+public :
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GameplayEffects")
+	FGameplayTagContainer GameplayCueTags;
+};
