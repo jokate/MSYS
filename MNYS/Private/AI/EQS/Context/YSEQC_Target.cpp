04a68c3d5012a214d33e604424fd9adb700f4cda
jokate <kkkk4017@naver.com>
2026-07-14 23:27:47 +0900

    타겟팅 관련 Env Query Context 추가.

---
+++ MNYS/Private/AI/EQS/Context/YSEQC_Target.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/EQS/Context/YSEQC_Target.h"
+
+#include "AI/YSAIController.h"
+#include "AI/Component/YSAIPerceptionComponent.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+
+
+void UYSEQC_Target::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	AYSAIController* QueryOwner = Cast<AYSAIController>(QueryInstance.Owner.Get());
+	
+	if ( IsValid(QueryOwner) == false )
+	{
+		return;
+	}
+	
+
+	AActor* BestTargetActor = QueryOwner->MainPerceptionComponent->GetBestTargetActor();
+	
+	if ( IsValid(BestTargetActor) == false )
+	{
+		return;
+	}
+	
+	UEnvQueryItemType_Actor::SetContextHelper(ContextData, BestTargetActor);
+}
---
+++ MNYS/Public/AI/EQS/Context/YSEQC_Target.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "YSEQC_Target.generated.h"
+
+/**
+ * 현재 BestTarget 기준으로 Context를 잡을 것임.
+ */
+UCLASS()
+class MNYS_API UYSEQC_Target : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+public : 
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+};
